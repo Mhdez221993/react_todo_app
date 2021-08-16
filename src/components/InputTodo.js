e2eed66673a00d223ee8aae98c1fd2f63698b438
Moises <mhdezcoronado@gmail.com>
2021-08-16 16:01:51 +0200

    Add style to InputTodo commponent

--- src/components/InputTodo.js
+++ src/components/InputTodo.js
@@ -25,16 +25,17 @@ class InputTodo extends Component {
 
   render() {
     return (
-      <form onSubmit={this.handleSubmit}>
-        <input
+    <form onSubmit={this.handleSubmit} className="form-container">
+      <input
         type="text"
+        className="input-text"
         placeholder="Add todo..."
         value={this.state.title}
-        onChange={this.onChange}
         name="title"
-        />
-        <button>Submit</button>
-      </form>
+        onChange={this.onChange}
+      />
+      <button className="input-submit">Submit</button>
+    </form>
     )
   }
 }
