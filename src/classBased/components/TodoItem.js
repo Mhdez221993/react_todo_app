95e5eeabee452b21397bcb20e6614a5f36259f17
Moises <mhdezcoronado@gmail.com>
2021-08-23 14:54:00 +0200

    Add fultionality for checkbox & del button

--- src/classBased/components/TodoItem.js
+++ src/classBased/components/TodoItem.js
@@ -1,8 +1,21 @@
 import React from "react"
 
 class TodoItem extends React.Component {
+
   render() {
-    return <li>{this.props.todo.title}</li>
+    return (
+      <li>
+        <input
+        type='checkbox'
+        checked={ this.props.todo.completed }
+        onChange={() => this.props.handleChangeProps(this.props.todo.id) }
+        />
+        {this.props.todo.title}
+        <button onClick={ ()=> this.props.deleteTodoProps(this.props.todo.id) }>
+          Delete
+        </button>
+      </li>
+    )
   }
 }
 
