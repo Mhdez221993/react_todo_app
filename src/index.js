3e5e7bc226d7b7648073cfd37c2ec7aff04b50fa
Moises <mhdezcoronado@gmail.com>
2021-08-24 19:36:11 +0200

    Fix github pages

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom"
 import "./App.css"
 // import TodoContainer from "./classBased/components/TodoContainer"
 import TodoContainer from "./functionBased/components/TodoContainer"
-import { BrowserRouter as Router } from "react-router-dom"
+import { HashRouter as Router } from "react-router-dom"
 
 ReactDOM.render(
 <React.StrictMode>
